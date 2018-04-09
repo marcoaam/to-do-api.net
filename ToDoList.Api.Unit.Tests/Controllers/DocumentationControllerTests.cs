80afecfea772941ef3b514a5c7153513d24ee36f
Marco Araujo <Marco.Araujo@bglgroup.co.uk>
2018-04-09 09:12:44 +0100

    Add documentation controller with status action

---
+++ ToDoList.Api.Unit.Tests/Controllers/DocumentationControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using ToDoList.Api.Controllers;
+
+namespace ToDoList.Api.Unit.Tests.Controllers
+{
+    public class DocumentationControllerTests
+    {
+        [Test]
+        public void ShouldBeAsignableToController()
+        {
+            var controller = new DocumentationController();
+
+            controller.Should().BeAssignableTo<Controller>();
+        }
+
+        [Test]
+        public void ShouldBeDecoratedWithTheRouteAttributeWithApiRoute()
+        {
+            typeof(DocumentationController).Should().BeDecoratedWith<RouteAttribute>(routeAttribute => routeAttribute.Template == "api");
+        }
+
+        [Test]
+        public void Status_ShouldReturn200Response()
+        {
+            var documentationController = new DocumentationController();
+
+            var result = documentationController.Status() as OkResult;
+
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(200);
+        }
+    }
+}
---
+++ ToDoList.Api/Controllers/DocumentationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Api.Controllers
+{
+    [Route("api")]
+    public class DocumentationController : Controller
+    {
+        public IActionResult Status()
+        {
+            return Ok();
+        }
+    }
+}
